182c87556c7ffbaea416ddc1c07615a53821c14f
Hyun <166475283+uhk0708@users.noreply.github.com>
2024-05-17 09:02:53 +0900

    Add files via upload

---
+++ 알고리즘/정렬/알고리즘 (선택 정렬).py
@@ -0,0 +1,26 @@
+## 함수
+from random import randint
+
+def findMinIndex(ary):
+    minIdx = 0
+    for i in range(1, len(ary)):
+        if (ary[minIdx] > ary[i]):
+            minIdx = i
+    return minIdx
+
+
+## 변수
+before = [randint(30,190) for _ in range(8)]
+after = []
+
+
+## 메인
+print('정렬 전-->', before)
+for i in range(len(before)):
+    minPos = findMinIndex(before)
+    after.append(before[minPos])
+    del(before[minPos])
+
+
+
+print('정렬 후-->',after)
